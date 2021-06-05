8ea61efa7bda97e25823102875985a54b6b88475
Crossy-swift <crossworm2011@gmail.com>
2021-06-05 15:34:46 +0300

    Initial Commit

---
+++ ColorChangeUI/ColorChangeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeUIApp.swift
+//  ColorChangeUI
+//
+//  Created by Артем Репин on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorChangeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorChangeUI
+//
+//  Created by Артем Репин on 05.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
